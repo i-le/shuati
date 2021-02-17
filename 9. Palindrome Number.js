b1557a8269856df9ad3ca3071cc8539628cbec50
Stephen Zheng <65367318+i-le@users.noreply.github.com>
2021-02-16 22:45:08 -0500

    Update 9. Palindrome Number.js

--- 9. Palindrome Number.js
+++ 9. Palindrome Number.js
@@ -9,3 +9,18 @@ public class Solution {
         return x == y;
     }
 }
+
+js solution:
+
+const isPalindrome = x => {
+    if (x < 0) return false
+
+    let reversed = 0, y = x
+
+    while (y > 0) {
+        const lastDigit = y % 10
+        reversed = (reversed * 10) + lastDigit
+        y = (y / 10) | 0
+    }
+    return x === reversed
+}
