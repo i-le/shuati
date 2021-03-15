bdb7817dbb22124a6b4ce6c6f754df4f439575a8
Stephen Zheng <65367318+i-le@users.noreply.github.com>
2021-03-14 20:39:23 -0400

    Add files via upload

---
+++ 20. ValidParentheses.js
@@ -0,0 +1,18 @@
+20. Valid Parentheses
+Easy
+
+7002
+
+289
+
+Add to List
+
+Share
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+
+
