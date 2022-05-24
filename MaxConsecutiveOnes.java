a50f497a2531fcee79800cb48c40f64f383ea58f
SZ <65367318+i-le@users.noreply.github.com>
2022-05-23 22:55:18 -0400

    Create   MaxConsecutiveOnes.java

---
+++ MaxConsecutiveOnes.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int max = 0;
+        int count = 0;
+        
+       
+            for (int i = 0; i < nums.length; i ++) {
+                if (nums[i] == 0) {
+                    max = 0;
+                } else {
+                    max ++;
+                    count = Math.max(count, max);
+                }
+            
+                
+                
+        }
+        
+        return count;
+        
+}
+}
